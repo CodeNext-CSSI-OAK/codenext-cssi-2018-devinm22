6dd0426aae1fb73409284ac31256ce404eb40b11
Student <student@Admins-MacBook-Air.local>
2018-07-09 12:05:23 -0700

    completed the first challenge

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-// Author: FirstName LastName
+// Author: Devin Mateo-Walker
+
+console.log('Hello Code Next!');
+console.log('This is another line');
+console.log ('My name is Devin');
+console.log('I play basketball');
