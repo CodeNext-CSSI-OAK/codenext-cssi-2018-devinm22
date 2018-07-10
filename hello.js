462e3109d229461c2ee05f890daad802ea385396
Student <student@Admins-MacBook-Air.local>
2018-07-10 15:25:13 -0700

    added a question area so that you can put in a year

--- hello.js
+++ hello.js
@@ -1,6 +1,18 @@
 // Author: Devin Mateo-Walker
 
-console.log('Hello Code Next!');
-console.log('This is another line');
-console.log ('My name is Devin');
-console.log('I play basketball');
+
+// let readline = require ('readline-sync');
+//
+// let name = readline.question('What is your name? ');
+//
+// console.log('Have a nice day ' + name);
+
+
+let readline = require('readline-sync');
+
+let age = Number(readline.question('What is your age? '));
+
+// let ageAsAString = readline.question('What is your age? ');
+// let age = Number(ageAsAString);  
+
+console.log('In 2028 you will be ' + (age + 10) + ' years old. ');
