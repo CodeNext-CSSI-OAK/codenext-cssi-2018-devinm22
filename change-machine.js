a586013a4f1d657539ef2267a8874c2f791acddb
Student <student@Admins-MacBook-Air.local>
2018-07-10 16:44:50 -0700

    made change machine work

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,21 @@
-// Author: FirstName LastName
+// Author: Devin Mateo-Walker
+
+let readline = require('readline-sync');
+
+let centAsAString = readline.question("Enter cents");
+let cents = Number(centAsAString);
+
+let quarters = Math.floor(cents / 25);
+console.log(quarters + "quarters");
+cents = cents % 25;
+
+let dimes = Math.floor(cents / 10);
+console.log(dimes + "dimes");
+cents = cents % 10;
+
+let nickles = Math.floor(cents / 5);
+console.log(nickles + "nickles");
+cents = cents % 5;
+
+let pennies = cents;
+console.log(cents + "pennies");
