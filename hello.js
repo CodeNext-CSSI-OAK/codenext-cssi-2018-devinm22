3475ebff220c7ce0e2059d9f1abab5f7693ec393
Student <student@Admins-MacBook-Air.local>
2018-07-17 12:57:14 -0700

    update

--- hello.js
+++ hello.js
@@ -13,6 +13,6 @@ let readline = require('readline-sync');
 let age = Number(readline.question('What is your age? '));
 
 // let ageAsAString = readline.question('What is your age? ');
-// let age = Number(ageAsAString);  
+// let age = Number(ageAsAString);
 
 console.log('In 2028 you will be ' + (age + 10) + ' years old. ');
---
+++ loops-practice.js
@@ -0,0 +1,17 @@
+// let readline = require("readline-sync");
+//
+//
+// let correctPassword = "badabing";
+// let input = "";
+// let attempts = 3;
+// while(input !== correctPassword && tries > 0) {
+//     input = readline.question("Please enter the correct password: ");
+//     tries--;
+// }
+// if(tries === 0) {
+//   console.log("You've tried too many times, give it back to the owner.");
+// } else {
+//   console.log("Access Gained");
+// }
+
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
